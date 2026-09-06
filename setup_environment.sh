f6bdf1aa5ecd3d02fd6934aeea6e94c979537891
Ania Shatskikh <anapriselec@gmail.com>
2026-09-06 16:00:17 +0300

    Update setup_environment.sh

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 set -e
+
 echo "=== 1. Установка библиотек ==="
 pip install -r requirements.txt
 
-echo "=== 2. Сборка модулей pyparadigm ==="
+echo "=== 2. Сборка C++ модулей pyparadigm ==="
 if [ ! -d "Sigmorphon2018SharedTask" ]; then
     git clone https://github.com/AlexeySorokin/Sigmorphon2018SharedTask.git
 fi
-cd Sigmorphon2018SharedTask
+
+# 2.1 Компиляция aligner (заходим внутрь папки aligner)
+echo "Компиляция aligner..."
+cd Sigmorphon2018SharedTask/aligner
 python3 setup_aligner.py build_ext --inplace
-cd pyparadigm
+cd ../..
+
+# 2.2 Компиляция pyparadigm
+echo "Компиляция pyparadigm..."
+cd Sigmorphon2018SharedTask/pyparadigm
 python3 setup.py build_ext --inplace
 cd ../..
 
@@ -18,4 +26,4 @@ if [ ! -d "data" ]; then
     git clone https://github.com/ania3000/Ossetic-COT.git data
 fi
 
-echo "=== Готово! ==="
+echo "=== Готово! Все модули успешно скомпилированы. ==="
