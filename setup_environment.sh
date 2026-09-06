c0cf4b8c0eb8d1af79f0ca5b69f07b1540c2b8cb
Ania Shatskikh <anapriselec@gmail.com>
2026-09-06 15:55:02 +0300

    Update setup_environment.sh

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-# setup_environment.sh
-git clone https://github.com/AlexeySorokin/Sigmorphon2018SharedTask.git
+set -e
+echo "=== 1. Установка библиотек ==="
+pip install -r requirements.txt
+
+echo "=== 2. Сборка модулей pyparadigm ==="
+if [ ! -d "Sigmorphon2018SharedTask" ]; then
+    git clone https://github.com/AlexeySorokin/Sigmorphon2018SharedTask.git
+fi
 cd Sigmorphon2018SharedTask
 python3 setup_aligner.py build_ext --inplace
 cd pyparadigm
 python3 setup.py build_ext --inplace
 cd ../..
-export PYTHONPATH=$PYTHONPATH:$(pwd)/Sigmorphon2018SharedTask
+
+echo "=== 3. Загрузка обучающих данных из Ossetic-COT ==="
+if [ ! -d "data" ]; then
+    git clone https://github.com/ania3000/Ossetic-COT.git data
+fi
+
+echo "=== Готово! ==="
